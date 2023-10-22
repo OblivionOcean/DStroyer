f279a9082bd835b9da82dbac6866ab849a57e347
Chui-shen <chuishenx@oblivionocean.top>
2023-10-22 14:44:01 +0800

    0.2 Update: Bugs No More. New Algorithms Work.

--- DStroyer.py
+++ DStroyer.py
@@ -1,6 +1,6 @@
 import os
-import re
 import time
+import functools
 
 """
 Usage:
@@ -8,38 +8,42 @@ Usage:
 2. Input "y" and the naughty MacOS files will perish.
 """
 
-filenames = []
-def fileFinder(filename, rootdir): # https://zhuanlan.zhihu.com/p/547499385
-    for fileordir in os.scandir(rootdir):
+filelist = []
+
+def clear(detection, path="."):
+    # dir_list = os.listdir(path) # No longer used because of its slowness. (AVG: 8s for each scan.)
+    # for i in dir_list:
+    #     abspath = os.path.join(os.path.abspath(path), i)
+    #     if os.path.isfile(abspath):
+    #         if i.startswith(detection):
+    #             print("Detected:", abspath)
+    #             os.remove(abspath)
+    #     if os.path.isdir(abspath):
+    #         clear(detection, path=abspath)
+
+    for fileordir in os.scandir(path):
         if fileordir.is_dir():
-            fileFinder(filename, fileordir.path)
-        else:
-            if filename.find('*') >= 0:
-                regularstring = filename
-                if filename.rfind(r'.') > 0:
-                    regularstring = regularstring.replace(r'.', r'\.')
-                regularstring = regularstring.replace('*', r'[\w.]*')
-                if re.match(regularstring, fileordir.name):
-                    filenames.append(fileordir.path)
-                    print(fileordir.path)
-            else:
-                if fileordir.name == filename:
-                    filenames.append(fileordir.path)
-                    print(fileordir.path)
+            clear(detection, fileordir.path)
+        i = fileordir.path
+        abspath = os.path.join(os.path.abspath(path), i)
+        if os.path.isfile(abspath):
+            if i.startswith(detection):
+                print("Detected:", abspath)
+                filelist.append(i)
 
 if __name__ == '__main__':
-    starttime = time.time()
-    filenames.clear()
-    fileFinder("._*", ".")
-    fileFinder(".DS_Store", ".")
-    endtime = time.time()
-    print(f"Time Cost: {endtime-starttime:.3f}")
-    ret = input(f"Are you sure to delete all MacOS .DS_Store/._* files? {len(filenames)}[y/n]")
+    ret = input(f"Are you sure to delete all MacOS .DS_Store/._* files? [y/n] ")
     if ret == 'y':
-        print("Starting deleting:")
-        for delfile in filenames:
+        starttime = time.time()
+        clear(".DS_Store", path=".")
+        clear("._", path=".")
+        endtime = time.time()
+        print("Starting deleting...")
+        for delfile in filelist:
             try:
                 os.remove(delfile)
                 print(f"Deleted: {delfile}")
             except Exception as e:
                 print(f"Failed: {e}")
+        print(f"Time Cost: {endtime-starttime:.3f}")
+        print("Deletion completed.")
